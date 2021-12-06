b353eecd682b953ece4d8f2c44de057e467aeabf
Rajwinder Bhatoe <rajwinder.bhatoe@gmail.com>
2021-12-06 17:30:18 +0000

    add tables to sql

---
+++ database/pgdb.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS pastes;
+CREATE TABLE users(
+	id SERIAL PRIMARY KEY,
+  	username VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE pastes(
+  user_id INT NOT NULL,
+  paste_id SERIAL PRIMARY KEY,
+  title VARCHAR(50),
+  paste_body TEXT NOT NULL,
+  createdAt TIMESTAMP NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
